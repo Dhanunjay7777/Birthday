5b634f665a32e0630f32d3d1d69a53d0521aaa97
Dhanunjay7777 <147498414+Dhanunjay7777@users.noreply.github.com>
2024-06-15 16:43:24 +0530

    login.js

--- src/login.js
+++ src/login.js
@@ -4,21 +4,21 @@ function Login() {
     return (
         <div className='full-height'>
             <div className="card">
-                <img 
+            <img src="https://i0.wp.com/www.eventstodayz.com/wp-content/uploads/2020/09/birthday-cake-gif-animation.gif" alt="birthday cake"/>
+                {/* <img 
                     src="https://raw.githubusercontent.com/DenverCoder1/Responsive-Birthday-Card/main/birthday.svg" 
                     alt="birthday" 
                     className="birthday"
-                />
+                /> */}
                 <div className="text">
-                    <h1 style={{ color: 'purple' }}>Happy Birthday Lokeshwari</h1>
+                    <h1 style={{ color: 'purple' }}>Happy Birthday Tanisha Chowdary</h1>
 
                     <p style={{ color: 'blues' }}>I hope you have a wonderful birthday</p>
                     <p style={{ color: 'green' }}>
-                        Your presence has made my life richer and more vibrant. 
-                        You have a way of making every day brighter and every challenge easier to face. 
-                        Your wisdom and humor have been a source of comfort and inspiration to me, 
-                        and I cherish all the memories we have created together.
+                    From the moment you were born, you brought an immeasurable amount of joy and happiness into our lives. Watching you grow has been a journey filled with pride and endless cherished memories. Each year, you blossom more beautifully, and our love for you deepens beyond measure.
+                    On your special day, we want you to know how much you mean to us. Your laughter is our music
                     </p>
+                    
                 </div>
                 <div className="space"></div>
             </div>
